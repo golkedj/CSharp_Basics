6fc7637ede88b66b2656a48890656f185f92fdc5
golkedj <golkedj@gmail.com>
2017-06-07 11:47:20 -0500

    Section 4, Lecture 32

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -6,25 +6,21 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var numbers = new int[3];
-            numbers[0] = 1;
+            var firstName = "Dave";
+            var lastName = "Golke";
 
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(numbers[2]);
+            var fullName = firstName + " " + lastName;
 
-            var flags = new bool[3];
-            flags[0] = true;
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
 
-            Console.WriteLine(flags[0]);
-            Console.WriteLine(flags[1]);
-            Console.WriteLine(flags[2]);
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
 
-            var names = new string[3] { "Jack", "John", "Mary" };
-
-            Console.WriteLine(names[0]);
-            Console.WriteLine(names[1]);
-            Console.WriteLine(names[2]);
+            var text = @"Hi John
+Look into the following paths
+c:\folder3\folder4
+c:\folder3\folder4";
+            Console.WriteLine(text);
         }
     }
 }
