6dba0af3b166721a305b666a12f4016ede0d85f7
golkedj <golkedj@gmail.com>
2017-06-09 11:33:41 -0500

    Section 5, Lecture 46

--- Iterations/Iterations/Program.cs
+++ Iterations/Iterations/Program.cs
@@ -10,22 +10,18 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            //var name = "John Smith";
-            //for (var i = 0; i < name.Length; i++)
-            //{
-            //    Console.WriteLine(name[i]);
-            //}
-
-            //foreach (var character in name)
-            //{
-            //    Console.WriteLine(character);
-            //}
-
-            var numbers = new int[] { 1, 2, 3, 4 };
-
-            foreach (var number in numbers)
+            while (true)
             {
-                Console.WriteLine(number);
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
             }
         }
     }
