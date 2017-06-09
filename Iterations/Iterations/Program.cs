32bb805daa4fc6192289ee5bb2ae1f77092c14c9
golkedj <golkedj@gmail.com>
2017-06-09 11:28:00 -0500

    Section 5, Lecture 45

--- Iterations/Iterations/Program.cs
+++ Iterations/Iterations/Program.cs
@@ -10,20 +10,22 @@ namespace Iterations
     {
         static void Main(string[] args)
         {
-            for (var i = 1; i <= 10; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
+            //var name = "John Smith";
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
 
-            for (var i = 10; i >= 1; i--)
+            //foreach (var character in name)
+            //{
+            //    Console.WriteLine(character);
+            //}
+
+            var numbers = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in numbers)
             {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine(number);
             }
         }
     }
