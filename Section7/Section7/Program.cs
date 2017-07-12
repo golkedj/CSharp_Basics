b1f7396d6dee1d5be53b9f2310d6be9ebe16aa76
golkedj <golkedj@gmail.com>
2017-07-12 11:49:45 -0500

    Section 7, Lecture 59

--- Section7/Section7/Program.cs
+++ Section7/Section7/Program.cs
@@ -11,7 +11,7 @@ namespace Section7
         static void Main(string[] args)
         {
             /* Lecture 58 */
-            var dateTime = new DateTime(2015, 1, 1);
+            /*var dateTime = new DateTime(2015, 1, 1);
             var now = DateTime.Now;
             var today = DateTime.Today;
 
@@ -25,7 +25,33 @@ namespace Section7
             Console.WriteLine(now.ToShortDateString());
             Console.WriteLine(now.ToLongTimeString());
             Console.WriteLine(now.ToShortTimeString());
-            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));*/
+
+            /* Lecture 59 */
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
+
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            // Properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            // Add
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+
+            // Subtract
+            Console.WriteLine("Add Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
+
+            // ToString
+            Console.WriteLine("ToString: " + timeSpan.ToString());
+
+            // Parse
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
         }
     }
 }
