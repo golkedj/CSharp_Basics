87c052f56fd34bd18a7114f0cc282d3c498768b1
golkedj <golkedj@gmail.com>
2017-07-16 12:52:43 -0500

    Section 8, Lecture 68

--- Section8/Section8/Exercises.cs
+++ Section8/Section8/Exercises.cs
@@ -8,13 +8,29 @@ namespace Section8
 {
     class Exercises
     {
+        /* 1- Write a program and ask the user to enter a few numbers separated by a hyphen. 
+         * Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" 
+         * or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive". */
         public static void Exercise1()
         {
-            /* 1- Write a program and ask the user to enter a few numbers separated by a hyphen. 
-             * Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" 
-             * or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive". */
+            
             Console.Write("Enter numbers seperated by hyphens: ");
             var inputString = Console.ReadLine();
+
+            
+            if (IsConsecutive(inputString))
+            {
+                Console.WriteLine("Consecutive");
+            }
+            else
+            {
+                Console.WriteLine("Not Consecutive");
+            }
+
+        }
+
+        public static bool IsConsecutive(string inputString)
+        {
             var numberChars = inputString.Split('-');
 
             var numbers = new List<int>();
@@ -46,54 +62,20 @@ namespace Section8
                     }
                 }
             }
-            if (consecutive)
-            {
-                Console.WriteLine("Consecutive");
-            }
-            else
-            {
-                Console.WriteLine("Not Consecutive");
-            }
 
+            return consecutive;
         }
 
+        /* 2- Write a program and ask the user to enter a few numbers separated by a hyphen. 
+         * If the user simply presses Enter, without supplying an input, exit immediately; 
+         * otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console. */
         public static void Exercise2()
         {
-            /* 2- Write a program and ask the user to enter a few numbers separated by a hyphen. 
-             * If the user simply presses Enter, without supplying an input, exit immediately; 
-             * otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console. */
             Console.Write("Enter numbers seperated by hyphens: ");
             var inputString = Console.ReadLine();
             if (!String.IsNullOrWhiteSpace(inputString))
             {
-                var numberChars = inputString.Split('-');
-
-                var numbers = new List<int>();
-
-                foreach (var number in numberChars)
-                {
-                    numbers.Add(Convert.ToInt32(number));
-                }
-
-                var duplicate = false;
-
-                for (var i = 0; i < (numbers.Count - 1); i++)
-                {
-                    for (var j = i + 1; j < numbers.Count; j++)
-                    {
-                        if (numbers[i] == numbers[j])
-                        {
-                            duplicate = true;
-                            break;
-                        }
-                    }
-                    if (duplicate)
-                    {
-                        break;
-                    }
-                }
-
-                if (duplicate)
+                if (ContainsDuplicates(inputString))
                 {
                     Console.WriteLine("Duplicate");
                 }
@@ -105,15 +87,45 @@ namespace Section8
 
         }
 
+        public static bool ContainsDuplicates(string inputString)
+        {
+            var numberChars = inputString.Split('-');
+
+            var numbers = new List<int>();
+
+            foreach (var number in numberChars)
+            {
+                numbers.Add(Convert.ToInt32(number));
+            }
+
+            var duplicate = false;
+
+            for (var i = 0; i < (numbers.Count - 1); i++)
+            {
+                for (var j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        duplicate = true;
+                        break;
+                    }
+                }
+                if (duplicate)
+                {
+                    break;
+                }
+            }
+
+            return duplicate;
+        }
+
+        /* 3- Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00). 
+         * A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; otherwise, display 
+         * "Invalid Time". If the user doesn't provide any values, consider it as invalid time. */
         public static void Exercise3()
         {
-            /* 3- Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00). 
-             * A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; otherwise, display 
-             * "Invalid Time". If the user doesn't provide any values, consider it as invalid time. */
             Console.Write("Enter a time value in the 24-hour time format (e.g. 19:00): ");
             var inputString = Console.ReadLine();
-            const int hours = 0;
-            const int minutes = 1;
             var valid = true;
             if (String.IsNullOrWhiteSpace(inputString))
             {
@@ -121,17 +133,7 @@ namespace Section8
             }
             else
             {
-                var timeSegs = inputString.Split(':');
-                var timeSegNums = new int[2];
-
-                timeSegNums[hours] = Convert.ToInt32(timeSegs[hours]);
-                timeSegNums[minutes] = Convert.ToInt32(timeSegs[minutes]);
-
-                if (!(((timeSegNums[hours] >= 0) && (timeSegNums[minutes] >= 0)) && 
-                    ((timeSegNums[hours] <= 23) && (timeSegNums[minutes] <= 59))))
-                {
-                    valid = false;
-                }
+                valid = IsValidTime(inputString);
             }
 
             if (valid)
@@ -146,14 +148,38 @@ namespace Section8
 
         }
 
+        public static bool IsValidTime(string inputString)
+        {
+            const int hours = 0;
+            const int minutes = 1;
+            var timeSegs = inputString.Split(':');
+            var timeSegNums = new int[2];
+
+            timeSegNums[hours] = Convert.ToInt32(timeSegs[hours]);
+            timeSegNums[minutes] = Convert.ToInt32(timeSegs[minutes]);
+
+            if (!(((timeSegNums[hours] >= 0) && (timeSegNums[minutes] >= 0)) &&
+                ((timeSegNums[hours] <= 23) && (timeSegNums[minutes] <= 59))))
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+        /* 4- Write a program and ask the user to enter a few words separated by a space. Use the words to 
+         * create a variable name with PascalCase. For example, if the user types: "number of students", 
+         * display "NumberOfStudents". Make sure that the program is not dependent on the input. So, if 
+         * the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents". */
         public static void Exercise4()
         {
-            /* 4- Write a program and ask the user to enter a few words separated by a space. Use the words to 
-             * create a variable name with PascalCase. For example, if the user types: "number of students", 
-             * display "NumberOfStudents". Make sure that the program is not dependent on the input. So, if 
-             * the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents". */
             Console.Write("Enter a few words separated by spaces: ");
             var inputString = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("PascalString: " + ConvertToPascal(inputString));
+        }
+
+        public static string ConvertToPascal(string inputString)
+        {
             var words = inputString.Split(' ');
             var pascalString = new StringBuilder();
             foreach (var word in words)
@@ -161,15 +187,22 @@ namespace Section8
                 pascalString.Append(word.ToString().Substring(0, 1).ToUpper());
                 pascalString.Append(word.ToString().ToLower().Substring(1, word.ToString().Length - 1));
             }
-            Console.WriteLine("PascalString: " + pascalString);
+
+            return pascalString.ToString();
         }
 
+        /* 5- Write a program and ask the user to enter an English word. Count the number of vowels (a, e, o, u, i) in the 
+         * word. So, if the user enters "inadequate", the program should display 6 on the console. */
         public static void Exercise5()
         {
-            /* 5- Write a program and ask the user to enter an English word. Count the number of vowels (a, e, o, u, i) in the 
-             * word. So, if the user enters "inadequate", the program should display 6 on the console. */
             Console.Write("Enter a word in English: ");
             var inputString = Console.ReadLine();
+
+            Console.WriteLine("Number of vowels was: " + CountVowels(inputString));
+        }
+
+        public static int CountVowels(string inputString)
+        {
             var vowels = 0;
             foreach (var ch in inputString.ToLower())
             {
@@ -187,7 +220,7 @@ namespace Section8
                 }
             }
 
-            Console.WriteLine("Number of vowels was: " + vowels);
+            return vowels;
         }
     }
 }
--- Section8/Section8/Program.cs
+++ Section8/Section8/Program.cs
@@ -65,6 +65,58 @@ namespace Section8
             Exercises.Exercise3();
             Exercises.Exercise4();
             Exercises.Exercise5();
+
+            /* Lecture 68 */
+            /*Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Reversed name: " + ReverseName(name));
+
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a number (or 'Quit' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                numbers.Add(Convert.ToInt32(input));
+            }
+
+            Console.WriteLine("Unique Numbers:");
+            foreach (var number in GetUniqueNumbers(numbers))
+            {
+                Console.WriteLine(number);
+            }*/
+        }
+
+        public static List<int> GetUniqueNumbers(List<int> numbers)
+        {
+            var uniques = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniques.Contains(number))
+                {
+                    uniques.Add(number);
+                }
+            }
+
+            return uniques;
+        }
+
+        public static string ReverseName(string name)
+        {
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+            {
+                array[name.Length - i] = name[i - 1];
+            }
+
+            return new string(array);
         }
     }
 }
